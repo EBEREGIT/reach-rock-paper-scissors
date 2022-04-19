a678d3149f81a46b948a31acbcfb7d9f81d78cb6
eberegit <samsoneberenjoku@gmail.com>
2022-04-19 14:07:36 +0100

    bets-and-wagers: added money to each participant to win all or loose or draw

--- index.mjs
+++ index.mjs
@@ -2,23 +2,40 @@ import { loadStdlib } from "@reach-sh/stdlib";
 import * as backend from "./build/index.main.mjs";
 const stdlib = loadStdlib();
 
+// setup an account balance for both participants
 const startingBalance = stdlib.parseCurrency(100);
 const accAlice = await stdlib.newTestAccount(startingBalance);
 const accBob = await stdlib.newTestAccount(startingBalance);
 
+// function to format the currency to 4 decimal places
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+
+// function to get the initial balace of a player before the game begins
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+
+// the account balances of the participants
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
+
+// contact deployment
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+// Hand and outcome variable definition
 const HAND = ["Rock", "Paper", "Scissors"];
 const OUTCOME = ["Bob wins", "Draw", "Alice wins"];
 
+// Player function definition
+// contains things that the participants can do
 const Player = (Who) => ({
+  // gets the hand the participant has decided to show
   getHand: () => {
     const hand = Math.floor(Math.random() * 3);
     console.log(`${Who} played ${HAND[hand]}`);
     return hand;
   },
 
+  // reveals the outcome of their choice of hand
   seeOutcome: (outcome) => {
     console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
   },
@@ -27,8 +44,18 @@ const Player = (Who) => ({
 await Promise.all([
   ctcAlice.p.Alice({
     ...Player("Alice"),
+    wager: stdlib.parseCurrency(5),
   }),
   ctcBob.p.Bob({
-    ...Player("Bob"),
+    ...Player('Bob'),
+    acceptWager: (amt) => {
+      console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+    },
   }),
 ]);
+
+const afterAlice = await getBalance(accAlice);
+const afterBob = await getBalance(accBob);
+
+console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
+console.log(`Bob went from ${beforeBob} to ${afterBob}.`);
--- index.rsh
+++ index.rsh
@@ -10,37 +10,48 @@ export const main = Reach.App(() => {
   // define the those participating in the game/program
   const Alice = Participant("Alice", {
     ...Player,
+    wager: UInt,
   });
   const Bob = Participant("Bob", {
     ...Player,
+    acceptWager: Fun([UInt], Null),
   });
 
-  //   start the program/game
+  // start the program/game
   init();
 
   // write your program here
   // Alice (the first participant) makes a decision as to which hand to show
   Alice.only(() => {
+    const wager = declassify(interact.wager);
+
     // makes the hand available to be seen (public) by others in the network
     const handAlice = declassify(interact.getHand());
   });
 
   //   Alice actually shows the hand
-  Alice.publish(handAlice);
+  Alice.publish(wager, handAlice).pay(wager);
   //   Adds to the network
   commit();
 
   // Bob repeats what Alice did
   Bob.only(() => {
+    interact.acceptWager(wager);
+
+    // makes the hand available to be seen (public) by others in the network
     const handBob = declassify(interact.getHand());
   });
-  Bob.publish(handBob);
+  Bob.publish(handBob).pay(wager);
 
   //   The program calculates the outcome of the game
   const outcome = (handAlice + (4 - handBob)) % 3;
+  const [forAlice, forBob] =
+    outcome == 2 ? [2, 0] : outcome == 0 ? [0, 2] : /* tie      */ [1, 1];
+  transfer(forAlice * wager).to(Alice);
+  transfer(forBob * wager).to(Bob);
   commit();
 
-//   send the outcome or result of each participant to the FE
+  //   send the outcome or result of each participant to the FE
   each([Alice, Bob], () => {
     interact.seeOutcome(outcome);
   });
